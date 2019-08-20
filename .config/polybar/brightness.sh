8332ef6fa39a5b5c33668ed9a8139e4419647b6f
benchislett <chislett.ben@gmail.com>
2019-08-21 00:21:01 +0900

    Add brightness to polybar

---
+++ .config/polybar/brightness.sh
@@ -0,0 +1,14 @@
+val=$(light -G)
+ival=${val%.*}
+
+if [ "$ival" -le "20" ];then
+  echo " $ival"
+elif [ "$ival" -le "40" ];then
+  echo " $ival"
+elif [ "$ival" -le "60" ];then
+  echo " $ival"
+elif [ "$ival" -le "80" ];then
+  echo " $ival"
+else
+  echo " $ival"
+fi
