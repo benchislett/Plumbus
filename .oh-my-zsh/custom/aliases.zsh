9c64be726637942c8609ff85e22bfd993ab65c7c
benchislett <chislett.ben@gmail.com>
2019-12-03 21:55:46 -0500

    Update status aliases to keep previous lines

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,10 +1,10 @@
 alias c='nocorrect clear'
 alias ls='ls -AFHv --color=tty'
-alias status-no-git='clear && ls'
-alias status='(git rev-parse --git-dir && clear && git status && echo && ls) || (status-no-git)'
+alias status-no-git='ls'
+alias status='(git rev-parse --git-dir && git status && echo && ls) || (status-no-git)'
 alias s='status'
-alias superstatus-no-git='clear && neofetch && ls'
-alias superstatus='(git rev-parse --git-dir && clear && neofetch && git status && echo && ls) || (superstatus-no-git)'
+alias superstatus-no-git='neofetch && ls'
+alias superstatus='(git rev-parse --git-dir && neofetch && git status && echo && ls) || (superstatus-no-git)'
 alias ss='superstatus'
 
 # XDG
